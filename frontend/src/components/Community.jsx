0738e167a4bfbbbcea54f93c38b1c912536027a7
Aryan9901 <911aaryan@gmail.com>
2024-04-07 11:06:05 +0530

    final commit image correction

--- frontend/src/components/Community.jsx
+++ frontend/src/components/Community.jsx
@@ -8,16 +8,16 @@ function Community() {
 			</p>
 			<div className={styles.container}>
 				<div className={styles.box1}>
-					<img src="assets/com11.jpg" alt="promotion image 1" />
+					<img src="/assets/com11.jpg" alt="promotion image 1" />
 					<div className={styles.line1}></div>
 				</div>
 				<div className={styles.box2}>
-					<img src="assets/com12.jpg" alt="promotion image 2" />
+					<img src="/assets/com12.jpg" alt="promotion image 2" />
 					{/* <div className={styles.line2}></div> */}
 					{/* <div className={styles.line3}></div> */}
 				</div>
 				<div className={styles.box3}>
-					<img src="assets/com13.jpg" alt="promotion image 3" />
+					<img src="/assets/com13.jpg" alt="promotion image 3" />
 					<div className={styles.line4}></div>
 				</div>
 			</div>
