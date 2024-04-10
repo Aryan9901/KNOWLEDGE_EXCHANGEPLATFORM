ec1a332bdfd3c40981b4de56b9c892b3113e49a4
ARYAN GUPTA <911aaryan@gmail.com>
2024-04-10 07:08:33 +0530

    Update Disscussion.jsx

--- frontend/src/pages/Disscussion.jsx
+++ frontend/src/pages/Disscussion.jsx
@@ -1,10 +1,10 @@
 // import styles from "../cssmodules/Disscussion.module.css";
 function Disscussion() {
-	return (
-		<>
-			<h1>disscussion page</h1>
-		</>
-	);
+return (
+	<>
+		<h1>disscussion page</h1>
+	</>
+);
 }
 
 export default Disscussion;
